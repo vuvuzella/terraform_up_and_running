ce0cae9632ae1964a94b9fc4811e50d1e0cbff77
vuvuzella <j.tabacii@gmail.com>
2021-08-07 00:32:35 +1000

    deploy an EC2 instance with a webserver

--- launch_ec2_sydney.tf
+++ launch_ec2_sydney.tf
@@ -9,4 +9,29 @@ resource "aws_instance" "example-terraform" {
     tags = {
         Name = "terraform-example"
     }
+    // give ec2 instance a start script using heredoc
+    user_data = <<-EOF
+        #!/bin/bash
+        echo "Hello, World" > index.html
+        nohup busybox httpd -f -p 8080 &
+        EOF
+    // reference the security group by using resource attribute referemce
+    // <provider>_<type>.<name>.<attribute>
+    // this creates an implicit dependency
+    vpc_security_group_ids = [aws_security_group.instance.id]
+}
+
+resource "aws_security_group" "instance" {
+    name = "terraform-example-instance"
+    ingress = [{
+      cidr_blocks = [ "0.0.0.0/0" ]
+      ipv6_cidr_blocks = ["::/0"]
+      description = "accept all incoming traffic from all ip addresses"
+      from_port = 8080
+      protocol = "tcp"
+      to_port = 8080
+      prefix_list_ids = []
+      security_groups = []
+      self = true
+    } ]
 }
