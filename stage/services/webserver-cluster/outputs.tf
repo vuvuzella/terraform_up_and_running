21e06ae2930ae30409bf1b090d18e3d8839ee5e5
vuvuzella <j.tabacii@gmail.com>
2021-09-18 22:49:12 +1000

    applied to cluster of servers

--- stage/services/webserver-cluster/outputs.tf
+++ stage/services/webserver-cluster/outputs.tf
@@ -1,21 +1,8 @@
-// output "public_ip" {
-// output "alb_dns_name" {
-//     // value = aws_instance.example.public_ip
-//     // description = "The public ip address of the web server"
-//     // use alb
-//     value = aws_lb.example.dns_name
-//     description = "The public ip address of the load balancer"
-// }
-// 
-// output "s3_bucket_arn" {
-//   value = aws_s3_bucket.terraform_state.arn
-//   description = "The ARN of the S3 bucket"
-// }
-// 
-// output "dynamodb_table_name" {
-//   value = aws_dynamodb_table.terraform_locks.name
-//   description = "The name of the DynamoDB table"
-// }
-output "ec2_instance_public_ip" {
-  value = aws_instance.example.public_ip
+output "alb_dns_name" {
+    value = aws_lb.example.dns_name
+    description = "The public ip address of the load balancer"
 }
+
+// output "ec2_instance_public_ip" {
+//   value = aws_instance.example.public_ip
+// }
